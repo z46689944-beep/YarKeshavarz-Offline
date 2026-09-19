ac6644ecf2ffb4f1381886aefc5f33960a12a99c
z46689944-beep <z46689944@gmail.com>
2026-09-19 22:16:20 +0330

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yar-keshavarz-offline-v1';
+const CACHE='yar-keshavarz-offline-v2';
 const CORE=['./','./index.html','./app.js','./sw.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo.png','./wheat-hero.jpg','./offline/offline-ai.js','./offline/agriculture-db.js','./admin.html','./admin.js','./admin.css','./admin-reset.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
